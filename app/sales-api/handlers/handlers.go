3b21965a5ab2eadfa4d65a616cf9ce3ad4781091
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2021-08-12 12:01:35 +0400

    add Logger function that logs the information before and after calling the handler

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -7,13 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/arammikayelyan/web-service-in-go/business/mid"
 	"github.com/arammikayelyan/web-service-in-go/foundation/web"
 )
 
 // API contructs an http.Handler with all application routes defined.
 func API(build string, shutdown chan os.Signal, log *log.Logger) *web.App {
 
-	app := web.NewApp(shutdown)
+	app := web.NewApp(shutdown, mid.Logger(log))
 
 	check := check{
 		log: log,
